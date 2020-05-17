7ef164e34cf97824548aa8a28df6915d1586f364
Skopin_mi@mail.ru <Skopin1981>
2020-05-17 09:03:58 +0300

    Homework lesson3

---
+++ src/ru/geekbrains/skopin/lesson3/Homework1.java
@@ -0,0 +1,45 @@
+package ru.geekbrains.skopin.lesson3;
+
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main(String[] args) {
+
+        boolean play = true;
+        Scanner sc = new Scanner(System.in);
+        int numberTry = 3;
+        int maxNumber = 9;
+        while (play) {
+            System.out.println("Угадайте загаданное число от 0 до " + maxNumber + ".");
+            System.out.println("У Вас " + numberTry + " попытки.");
+            sayResult(playGame(sc, numberTry, maxNumber));
+            play = isNextGame(sc);
+        }
+        System.out.println("Игра закончена!");
+        sc.close();
+    }
+
+    public static boolean playGame (Scanner sc, int numberTry, int maxNumber) { //логика игры результат победа да/нет
+        boolean win = false;
+        int number = (int) (Math.random() * (maxNumber + 1));
+        for (int i = 0; i < numberTry; i++) {
+            int userNumber = sc.nextInt();
+            if (number == userNumber) {
+                win = true;
+                break;
+            } else {
+                System.out.println(":( Не верно!");
+                System.out.println(number > userNumber ? "Загаданное число больше Вашего." : "Загаданное число меньше Вашего.");
+            }
+        }
+        return win;
+    }
+    public static void sayResult (Boolean win){ //печать результата
+        System.out.println(win?"Вы угадали!":"Вы не угадали!");
+    }
+    public static Boolean isNextGame (Scanner sc) { // запрос продолжать ли игру?
+        System.out.println("Попробуйте еще раз?");
+        System.out.println("Повторить игру еще раз? 1 - да / 0 - нет.");
+        return sc.nextInt() != 0;
+    }
+}
---
+++ src/ru/geekbrains/skopin/lesson3/Homework2.java
@@ -0,0 +1,55 @@
+package ru.geekbrains.skopin.lesson3;
+
+import java.util.Scanner;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] words = {"apple", "orange", "lemon", "banana",
+                "apricot", "avocado", "broccoli", "carrot", "cherry",
+                "garlic", "grape", "melon", "leak", "kiwi", "mango",
+                "mushroom", "nut", "olive", "pea", "peanut", "pear",
+                "pepper", "pineapple", "pumpkin", "potato"
+        };
+        int indexWord = (int) (Math.random() * 25);
+        Boolean win;
+        String thisWord = words[indexWord];
+        System.out.println("Угадайте загаданное слово.");
+        String userWord;
+        do {
+            userWord = sc.nextLine();
+            win = compareTwoWords(thisWord, userWord);
+            if (!win) {
+                printChar(thisWord, userWord);
+                System.out.println("Попробуйте еще.");
+            }
+            sayResult(win);
+        } while (!win);
+        sc.close();
+    }
+
+    public static void sayResult (Boolean win){ //печать результата
+        System.out.println(win?"Вы угадали!":"Вы не угадали!");
+    }
+
+    public static Boolean compareTwoWords (String thisWord, String userWord) {  // сравнение слов да/нет
+        return thisWord.equals(userWord);
+    }
+
+    public static void printChar (String thisWord, String userWord){   // печать совпавших букв и #
+        int littleWord = Math.min(thisWord.length() , userWord.length()) ;
+        for (int i = 0; i < littleWord - 1; i++) {    // печать совпадений
+            char a = thisWord.charAt(i);
+            char b = userWord.charAt(i);
+            if (a == b) {
+                System.out.print(a);
+            } else {
+                System.out.print("#");
+            }
+        }
+        for (int i = 0; i < 16 - thisWord.length(); i++) { // допечатываем до 15 #
+            System.out.print("#");
+        }
+        System.out.println("");
+    }
+}
