1ad374a706c71eaf882bf83e9d32a25ee3f24ecb
Skopin_mi@mail.ru <Skopin1981>
2020-05-19 08:27:12 +0300

    отдельно в метод вынес загадывание числа

--- src/ru/geekbrains/skopin/lesson3/Homework1.java
+++ src/ru/geekbrains/skopin/lesson3/Homework1.java
@@ -5,24 +5,34 @@ import java.util.Scanner;
 public class Homework1 {
     public static void main(String[] args) {
 
-        boolean play = true;
+        for (int i = 0; i < 50; i++) {
+            System.out.print(guessTheNumber(9));
+        }
+        boolean continuePlay = true;
         Scanner sc = new Scanner(System.in);
         int numberTry = 3;
         int maxNumber = 9;
-        while (play) {
+        while (continuePlay) {
             System.out.println("Угадайте загаданное число от 0 до " + maxNumber + ".");
             System.out.println("У Вас " + numberTry + " попытки.");
-            sayResult(playGame(sc, numberTry, maxNumber));
-            play = isNextGame(sc);
+            boolean win = tryToGuess(sc, numberTry, maxNumber);
+            sayResult(win);
+            continuePlay = isNextGame(sc);
         }
         System.out.println("Игра закончена!");
         sc.close();
     }
 
-    public static boolean playGame (Scanner sc, int numberTry, int maxNumber) { //логика игры результат победа да/нет
+
+    private static int guessTheNumber (int maxNumber) {
+        int number = (int) (Math.random() * (maxNumber + 1));          // загадали число
+        return number;
+    }
+
+    private static boolean tryToGuess (Scanner sc, int numberTry, int maxNumber) { //логика игры результат победа да/нет
 
         boolean win = false;
-        int number = (int) (Math.random() * (maxNumber + 1));          // загадали число
+        int number = guessTheNumber(maxNumber);
         for (int i = 0; i < numberTry; i++) {
             int userNumber = sc.nextInt();
             if (number == userNumber) {                                // сравнение чисел
@@ -36,11 +46,11 @@ public class Homework1 {
         return win;
     }
 
-    public static void sayResult (Boolean win){ //печать результата
+    private static void sayResult (Boolean win){ //печать результата
         System.out.println(win?"Вы угадали!":"Вы не угадали!");
     }
 
-    public static Boolean isNextGame (Scanner sc) { // запрос продолжать ли игру?
+    private static Boolean isNextGame (Scanner sc) { // запрос продолжать ли игру?
         System.out.println("Попробуйте еще раз?");
         System.out.println("Повторить игру еще раз? 1 - да / 0 - нет.");
         return sc.nextInt() != 0; // получение ответа
