a7bd95b57256d1123e17a516dd4da250f193471a
Skopin_mi@mail.ru <Skopin1981>
2020-06-05 17:12:27 +0300

    Homework

---
+++ src/ru/geekbrains/skopin/lesson8/Main.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.skopin.lesson8;
+
+public class Main {
+/*
+    почти пусто ...
+ */
+    public static void main(String[] args) {
+
+        GameWindow gameWindow = GameWindow.createGameWindow();
+        gameWindow.setVisible(true);
+    }
+}
